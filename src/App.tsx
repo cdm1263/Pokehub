800c23971128d4c7d4d86b0d1ead4f2dfd12cf40
BearHumanS <bye0651@naver.com>
2023-11-01 14:08:22 +0900

    initialization 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Home';
+import Layout from '@/components/Layout';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/firebase.ts
@@ -0,0 +1,34 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, FirebaseApp, getApp } from 'firebase/app';
+import 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+export let app: FirebaseApp;
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGIN_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+};
+
+try {
+  app = getApp('app');
+} catch (error) {
+  app = initializeApp(firebaseConfig, 'app');
+}
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+
+export default firebase;
