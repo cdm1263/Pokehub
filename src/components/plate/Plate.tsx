7018b76dd5d0f16b7a63d8a7144d5c7a956a637d
ChaDongMin <cdm1263@gmail.com>
2023-11-04 16:03:47 +0900

    Feat: 타입의 선택 여부에 따른 렌더링 처리

--- src/components/plate/Plate.tsx
+++ src/components/plate/Plate.tsx
@@ -1,19 +1,51 @@
 import { POKEMON_TYPES } from '@/lib/constants';
 import styles from './Plate.module.scss';
+import useSelectedStore from '@/store/useSelectedStore';
 
 const Plate = () => {
   const koreanTypes = Object.values(POKEMON_TYPES);
+  const { selectedPlate, setSelectedPlate } = useSelectedStore();
 
-  return koreanTypes.map((koreanType) => (
-    <div
-      className={`${styles.plate} ${styles[koreanType]}`}
-      key={koreanType}
-      data-type={koreanType}
-    >
-      <img src="" alt={`${koreanType}타입 아이콘`} />
-      <span>{koreanType}</span>
-    </div>
-  ));
+  console.log(selectedPlate);
+
+  const renderTypes = (types: string[]) =>
+    types.map((koreanType) => {
+      if (selectedPlate.includes(koreanType)) {
+        return (
+          <div
+            onClick={() => {
+              setSelectedPlate(koreanType);
+            }}
+            className={`${styles.click_plate} ${styles[koreanType]}`}
+            key={koreanType}
+            data-type={koreanType}
+          >
+            <img
+              src={`/src/assets/${koreanType}.svg`}
+              alt={`${koreanType}타입 아이콘`}
+            />
+          </div>
+        );
+      }
+      return (
+        <div
+          onClick={() => {
+            setSelectedPlate(koreanType);
+          }}
+          className={`${styles.unclick_plate} ${styles[koreanType]}`}
+          key={koreanType}
+          data-type={koreanType}
+        >
+          <img
+            src={`/src/assets/${koreanType}.svg`}
+            alt={`${koreanType}타입 아이콘`}
+          />
+          <span>{koreanType}</span>
+        </div>
+      );
+    });
+
+  return <div className={styles.wrapper}>{renderTypes(koreanTypes)}</div>;
 };
 
 export default Plate;
