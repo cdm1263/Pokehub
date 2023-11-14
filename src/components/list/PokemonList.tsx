f4d0cd2687f3ffce3c9d241cdaca6147d74d1ea1
ChaDongmin <cdm1263@gmail.com>
2023-11-14 16:34:57 +0900

    Refactor: api 로직 간소화 및 데이터를 DOM에 전달

--- src/components/list/PokemonList.tsx
+++ src/components/list/PokemonList.tsx
@@ -1,35 +1,40 @@
-import { useEffect, useState } from 'react';
 import styles from './PokemonList.module.scss';
 import { useGetAllPokemon } from '@/query/qeuries';
-import { fetchData } from '@/lib/api';
+import { reverseObject } from '@/lib/utill/reverseObject';
+import { POKEMON_NAME } from '@/lib/pokemonName';
+import { POKEMON_TYPES } from '@/lib/constants';
+import { TypesType } from '@/lib/type';
 
 const PokemonList = () => {
-  const { isLoading, data } = useGetAllPokemon();
-  const [pokemonData, setPokemonData] = useState<Pokemon[]>([]);
-
-  interface Pokemon {
-    name: string;
-    url: string;
-  }
-
-  useEffect(() => {
-    if (!isLoading) {
-      const getAllPokemonDatas = async () => {
-        const pokemonDetailDatas = await Promise.all(
-          data.results.map((item: Pokemon) => fetchData(item.url, 'get')),
-        );
-        setPokemonData(pokemonDetailDatas);
-      };
-      getAllPokemonDatas();
-    }
-  }, [isLoading, data]);
+  const { isLoading, data: pokemonData } = useGetAllPokemon();
 
   if (isLoading) {
     return <div>Loading...</div>;
   }
 
-  console.log(pokemonData);
-  return <div className={styles.pokemon_grid}></div>;
+  return (
+    <div className={styles.pokemon_grid}>
+      {pokemonData?.map((data) => (
+        <div
+          key={data.name}
+          onClick={() => {
+            console.log(data.name, reverseObject(POKEMON_NAME)[data.name]);
+          }}
+        >
+          <span>{data.id}</span>
+          <img src={data.sprites.front_default} alt="" />
+          <div>
+            {data.types.map((typeData: TypesType) => (
+              <div key={typeData.type.name}>
+                {POKEMON_TYPES[typeData.type.name]}
+              </div>
+            ))}
+          </div>
+          <span>{reverseObject(POKEMON_NAME)[data.name]}</span>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default PokemonList;
