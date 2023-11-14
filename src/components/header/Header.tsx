2f722afee04a9ecc3acdfb785dbc7b912ffb30a3
BearHumanS <bye0651@naver.com>
2023-11-14 23:43:36 +0900

    Design: 헤더 스타일 변경

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,10 @@
 import { Link } from 'react-router-dom';
 import styles from './Header.module.scss';
+import SocialLogin from '../users/SocialLogin';
+import { FaRegAddressCard } from 'react-icons/fa6';
+import { BsFilePlus } from 'react-icons/bs';
+import { RiUserVoiceFill } from 'react-icons/ri';
+import { FiLogIn } from 'react-icons/fi';
 
 const Header = () => {
   return (
@@ -9,22 +14,49 @@ const Header = () => {
           <div>
             <Link to="/" className={styles.logo}>
               <img
-                src="src/assets/logo.png"
+                src="https://github.com/side-project-cdmnkh/my-pokemon/assets/115094069/fd0a37a3-f3ba-479c-a3f2-f58f55e0286c"
                 alt="logo"
-                width={100}
-                height={50}
+                width={132}
+                height={59}
               />
             </Link>
             <div className={styles.menu__group}>
+              <form className={styles.main__search}>
+                <label className={styles.search__inner}>
+                  <input
+                    type="text"
+                    placeholder="검색어를 입력해주세요."
+                    className={styles.main__search__bar}
+                  />
+                  <button type="submit" className={styles.search__btn}>
+                    <img src="/src/assets/search_icon.png" alt="" />
+                  </button>
+                </label>
+              </form>
               <nav className={styles.nav__box}>
-                <div>
-                  <Link to="/">도감</Link>
-                </div>
-                <div>
-                  <Link to={`/pokemon/${1}`}>카드 제작</Link>
+                <Link to="/">
+                  <div className={styles.nav__item}>
+                    <FaRegAddressCard />
+                    도감
+                  </div>
+                </Link>
+                <Link to={`/pokemon/${1}`}>
+                  <div className={styles.nav__item}>
+                    <BsFilePlus />
+                    카드 제작
+                  </div>
+                </Link>
+                <Link to={`/pokemon/${1}`}>
+                  <div className={styles.nav__item}>
+                    <RiUserVoiceFill />
+                    커뮤니티
+                  </div>
+                </Link>
+                <div className={styles.nav__item}>
+                  <FiLogIn />
+                  <SocialLogin />
                 </div>
               </nav>
-              <div className="util__menu">로그인</div>
             </div>
           </div>
         </div>
