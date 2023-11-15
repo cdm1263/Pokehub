7d6c0fcb311c7b0f3faed8ed9eaf4768ea67dcfd
BearHumanS <bye0651@naver.com>
2023-11-16 00:35:37 +0900

    Feat: 로그인 상태관리 및 라우터 관리

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,48 @@
+import { useEffect, useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { getAuth } from '@firebase/auth';
+import { app } from '@/firebase';
 import Home from './Home';
 import Layout from '@/components/Layout';
 import Detail from './pages/detail.tsx/detail';
+import useAuthState from './provider/authProvider';
+import useUserStore from './store/useUsersStore';
 
 const App = () => {
+  useAuthState();
+  const auth = getAuth(app);
+  const [init, setInit] = useState<boolean>(false);
+  const { user } = useUserStore();
+
+  console.log(user);
+
+  useEffect(() => {
+    setInit(true);
+  }, [auth]);
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/pokemon/:id" element={<Detail />} />
-        </Route>
-      </Routes>
+      {init ? (
+        <>
+          <Routes>
+            <Route element={<Layout />}>
+              {user ? (
+                <>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/pokemon/:id" element={<Detail />} />
+                </>
+              ) : (
+                <>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/pokemon/:id" element={<Detail />} />
+                </>
+              )}
+            </Route>
+          </Routes>
+        </>
+      ) : (
+        <>로딩ui추가</>
+      )}
     </BrowserRouter>
   );
 };
