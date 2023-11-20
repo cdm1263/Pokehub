8d579bea7e5a7767d8583a88a0e28dc3934a7631
BearHumanS <bye0651@naver.com>
2023-11-20 14:01:45 +0900

    FIX: conflict 해결

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,13 @@ import { useEffect, useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { getAuth } from '@firebase/auth';
 import { app } from '@/firebase';
-import Home from './Home';
+import DEX from '@/pages/dex/Dex';
 import Layout from '@/components/Layout';
 import Detail from './pages/detail.tsx/detail';
-<<<<<<< Updated upstream
 import { QueryClientProvider } from 'react-query';
 import { queryClient } from './query/queryClient';
-=======
 import useAuthState from './provider/authProvider';
 import useUserStore from './store/useUsersStore';
->>>>>>> Stashed changes
 
 const App = () => {
   useAuthState();
@@ -27,29 +24,27 @@ const App = () => {
 
   return (
     <BrowserRouter>
-<<<<<<< Updated upstream
       <QueryClientProvider client={queryClient}>
         <Routes>
           <Route element={<Layout />}>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<DEX />} />
             <Route path="/pokemon/:id" element={<Detail />} />
           </Route>
         </Routes>
       </QueryClientProvider>
-=======
       {init ? (
         <>
           <Routes>
             <Route element={<Layout />}>
               {user ? (
                 <>
-                  <Route path="/" element={<Home />} />
+                  <Route path="/" element={<DEX />} />
                   <Route path="/pokemon/:id" element={<Detail />} />
                   {/*나중에 프로필 라우터 추가*/}
                 </>
               ) : (
                 <>
-                  <Route path="/" element={<Home />} />
+                  <Route path="/" element={<DEX />} />
                   <Route path="/pokemon/:id" element={<Detail />} />
                 </>
               )}
@@ -59,7 +54,6 @@ const App = () => {
       ) : (
         <>로딩 중</>
       )}
->>>>>>> Stashed changes
     </BrowserRouter>
   );
 };
