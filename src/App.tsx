a7aeff43e93619893826f272a45bfc22fb957708
BearHumanS <bye0651@naver.com>
2023-11-20 14:15:57 +0900

    Fix: confilct

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { getAuth } from '@firebase/auth';
-import { app } from '@/firebase';
+// import { getAuth } from '@firebase/auth';
+// import { app } from '@/firebase';
 import DEX from '@/pages/dex/Dex';
 import Layout from '@/components/Layout';
 import Detail from './pages/detail.tsx/detail';
 import { QueryClientProvider } from 'react-query';
 import { queryClient } from './query/queryClient';
-import useAuthState from './provider/authProvider';
-import useUserStore from './store/useUsersStore';
+// import useAuthState from './provider/authProvider';
+// import useUserStore from './store/useUsersStore';
 
 const App = () => {
-  useAuthState();
-  const auth = getAuth(app);
-  const [init, setInit] = useState<boolean>(false);
-  const { user } = useUserStore();
+  // useAuthState();
+  // const auth = getAuth(app);
+  // const [init, setInit] = useState<boolean>(false);
+  // const { user } = useUserStore();
 
-  console.log('user', user);
+  // console.log('user', user);
 
-  useEffect(() => {
-    setInit(true);
-  }, [auth]);
+  // useEffect(() => {
+  //   setInit(true);
+  // }, [auth]);
 
   return (
     <BrowserRouter>
@@ -32,30 +32,31 @@ const App = () => {
           </Route>
         </Routes>
       </QueryClientProvider>
-      {init ? (
-        <>
-          <Routes>
-            <Route element={<Layout />}>
-              {user ? (
-                <>
-                  <Route path="/" element={<DEX />} />
-                  <Route path="/pokemon/:id" element={<Detail />} />
-                  {/*나중에 프로필 라우터 추가*/}
-                </>
-              ) : (
-                <>
-                  <Route path="/" element={<DEX />} />
-                  <Route path="/pokemon/:id" element={<Detail />} />
-                </>
-              )}
-            </Route>
-          </Routes>
-        </>
-      ) : (
-        <>로딩 중</>
-      )}
     </BrowserRouter>
   );
 };
 
 export default App;
+
+// {init ? (
+//   <>
+//     <Routes>
+//       <Route element={<Layout />}>
+//         {user ? (
+//           <>
+//             <Route path="/" element={<DEX />} />
+//             <Route path="/pokemon/:id" element={<Detail />} />
+//             {/*나중에 프로필 라우터 추가*/}
+//           </>
+//         ) : (
+//           <>
+//             <Route path="/" element={<DEX />} />
+//             <Route path="/pokemon/:id" element={<Detail />} />
+//           </>
+//         )}
+//       </Route>
+//     </Routes>
+//   </>
+// ) : (
+//   <>로딩 중</>
+// )}
