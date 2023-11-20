93cced809e51eafbd32cb4d2311b96f653ad6f85
BearHumanS <bye0651@naver.com>
2023-11-21 06:51:14 +0900

    Feat: 헤더 로그인 시에 유저 프로필 이미지 추가 및 드롭다운 박스 온오프 관련

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,12 +5,27 @@ import { FaRegAddressCard } from 'react-icons/fa6';
 import { BsFilePlus } from 'react-icons/bs';
 import { RiUserVoiceFill } from 'react-icons/ri';
 import { FiLogIn } from 'react-icons/fi';
-import { FormEvent, useState } from 'react';
+import { FormEvent, useEffect, useRef, useState } from 'react';
+import useUserStore from '@/store/useUsersStore';
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const { user } = useUserStore();
+  const dropdownRef = useRef<HTMLDivElement | null>(null);
 
   const toggleDropdown = () => setIsOpen(!isOpen);
+
+  const onClickOutSide = (e: MouseEvent) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(e.target as Node))
+      setIsOpen(false);
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', onClickOutSide);
+    return () => {
+      document.removeEventListener('mousedown', onClickOutSide);
+    };
+  }, []);
 
   const onSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -46,24 +61,41 @@ const Header = () => {
               <nav className={styles.nav__box}>
                 <Link to="/">
                   <div className={styles.nav__item}>
-                    <FaRegAddressCard />
+                    <FaRegAddressCard size={31} />
                     도감
                   </div>
                 </Link>
                 <Link to={`/pokemon/${1}`}>
                   <div className={styles.nav__item}>
-                    <BsFilePlus />
+                    <BsFilePlus size={31} />
                     카드 제작
                   </div>
                 </Link>
                 <Link to={`/pokemon/${1}`}>
                   <div className={styles.nav__item}>
-                    <RiUserVoiceFill />
+                    <RiUserVoiceFill size={31} />
                     커뮤니티
                   </div>
                 </Link>
-                <div className={styles.nav__item} onClick={toggleDropdown}>
-                  <FiLogIn />
+                <div
+                  ref={dropdownRef}
+                  className={styles.nav__item}
+                  onClick={toggleDropdown}
+                >
+                  {user ? (
+                    <div style={{ width: '31px', height: '31px' }}>
+                      <img
+                        style={{ borderRadius: '50%' }}
+                        src={user?.photoURL || undefined}
+                        alt="프로필 사진"
+                        width={31}
+                        height={31}
+                      />
+                    </div>
+                  ) : (
+                    <FiLogIn size={31} />
+                  )}
+
                   <SocialLogin isOpen={isOpen} setIsOpen={setIsOpen} />
                 </div>
               </nav>
