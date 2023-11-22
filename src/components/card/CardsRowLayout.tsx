769f0a8c2c4448c8b66740a462c220180255d65c
ChaDongmin <cdm1263@gmail.com>
2023-11-22 20:16:31 +0900

    Feat: 포켓몬 선택 레이아웃 생성

---
+++ src/components/card/CardsRowLayout.tsx
@@ -0,0 +1,30 @@
+import { PokemonType } from '@/lib/type';
+import styles from './cards.module.scss';
+import PokemonSelectImage from './PokemonSelectImage';
+import { POKEMON_NAME } from '@/lib/pokemonName';
+import { reverseObject } from '@/lib/utill/reverseObject';
+
+interface CardsRowLayout {
+  pokemonArray: PokemonType[];
+}
+
+const CardsRowLayout = ({ pokemonArray }: CardsRowLayout) => {
+  return (
+    <div className={styles.cards__row_cards}>
+      {pokemonArray.map((randomPokemon) => (
+        <div className={styles.cards__row_cards__column}>
+          <PokemonSelectImage
+            key={randomPokemon?.id}
+            pokemonImage={
+              randomPokemon?.sprites?.other?.['official-artwork']?.front_default
+            }
+          />
+          <span>{reverseObject(POKEMON_NAME)[randomPokemon.name]}</span>
+          <button className={styles.border_button}>포켓몬 사용</button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default CardsRowLayout;
