9963924d4e871d5bb2d96a1240468fb8ada9ed00
BearHumanS <bye0651@naver.com>
2023-11-28 17:32:49 +0900

    Feat: 페이지 이동 시에  스크롤 최상단으로 이동하는 로직 추가

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,14 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from '@/components/header/Header';
 import Footer from '@/components/footer/Footer';
+import { useEffect } from 'react';
 
 const Layout = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
   return (
     <>
       <Header />
