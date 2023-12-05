06b45cfeab79c2bb4b4f6ac5e4c6f0acd3977ec7
ChaDongmin <cdm1263@gmail.com>
2023-12-05 11:36:18 +0900

    Feat: store 연결 및 사용 버튼 핸들러 연결

--- src/components/card/CardsRowLayout.tsx
+++ src/components/card/CardsRowLayout.tsx
@@ -3,12 +3,15 @@ import styles from './cards.module.scss';
 import PokemonSelectImage from './PokemonSelectImage';
 import { POKEMON_NAME } from '@/lib/pokemonName';
 import { reverseObject } from '@/lib/utill/reverseObject';
+import useSelectedPokemonForCard from '@/store/useSelectedPokemonForCard';
 
 interface CardsRowLayout {
   pokemonArray: PokemonType[];
 }
 
 const CardsRowLayout = ({ pokemonArray }: CardsRowLayout) => {
+  const { setPokemonData } = useSelectedPokemonForCard();
+
   return (
     <div className={styles.cards__row_cards}>
       {pokemonArray.map((randomPokemon) => (
@@ -22,7 +25,14 @@ const CardsRowLayout = ({ pokemonArray }: CardsRowLayout) => {
             }
           />
           <span>{reverseObject(POKEMON_NAME)[randomPokemon.name]}</span>
-          <button className={styles.border_button}>포켓몬 사용</button>
+          <button
+            className={styles.border_button}
+            onClick={() => {
+              setPokemonData(randomPokemon);
+            }}
+          >
+            포켓몬 사용
+          </button>
         </div>
       ))}
     </div>
