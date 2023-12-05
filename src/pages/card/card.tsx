1608ff20546857ca2b9af39ed24e924a3711165a
ChaDongmin <cdm1263@gmail.com>
2023-12-05 11:33:18 +0900

    Feat: PokemonCard 컴포넌트 적용 및 store 연결

--- src/pages/card/card.tsx
+++ src/pages/card/card.tsx
@@ -1,9 +1,13 @@
 import CardsRowLike from '@/components/card/CardsRowLike';
 import CardsRowRandom from '@/components/card/CardsRowRandom';
+import PokemonCard from '@/components/card/PokemonCard';
+import useSelectedPokemonForCard from '@/store/useSelectedPokemonForCard';
 
 const Card = () => {
+  const { pokemonData } = useSelectedPokemonForCard();
   return (
     <>
+      <PokemonCard data={pokemonData} />
       <CardsRowLike />
       <CardsRowRandom />;
     </>
