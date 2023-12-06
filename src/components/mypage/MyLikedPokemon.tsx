de6a7fb8f1aeeec462223956e270ad6ac8d6cee4
BearHumanS <bye0651@naver.com>
2023-12-06 17:43:49 +0900

    Feat: 찜리스트 전역으로 관리 수정

--- src/components/mypage/MyLikedPokemon.tsx
+++ src/components/mypage/MyLikedPokemon.tsx
@@ -1,44 +1,16 @@
-import { arrayRemove, doc, onSnapshot, updateDoc } from 'firebase/firestore';
+import { arrayRemove, doc, updateDoc } from 'firebase/firestore';
 import styles from './Mypage.module.scss';
 import { db } from '@/firebase';
 import useUserStore from '@/store/useUsersStore';
-import { useCallback, useEffect } from 'react';
-import { getPokemonData } from '@/lib/poketApi';
 import { useNavigate } from 'react-router-dom';
 import useLikedStore from '@/store/useLikedStore';
 import RenderPokemon from './RenderPokemon';
 
 const MyLikedPokemon = () => {
   const { user } = useUserStore();
-  const { pokemonData, setPokemonData } = useLikedStore();
+  const { pokemonData } = useLikedStore();
 
   const navigate = useNavigate();
-
-  const fetchAllLikedPokemons = useCallback(
-    async (likedPokemons: []) => {
-      try {
-        const LikedPokemonDataPromise = likedPokemons.map((id) =>
-          getPokemonData(id),
-        );
-        const data = await Promise.all(LikedPokemonDataPromise);
-        setPokemonData(data);
-      } catch (error) {
-        console.error(error);
-      }
-    },
-    [setPokemonData],
-  );
-
-  useEffect(() => {
-    if (user?.uid) {
-      const unsubscribe = onSnapshot(doc(db, 'likes', user.uid), (doc) => {
-        const likedPokemons = doc.data()?.pokemons || [];
-        fetchAllLikedPokemons(likedPokemons);
-      });
-
-      return () => unsubscribe();
-    }
-  }, [user?.uid, fetchAllLikedPokemons]);
 
   const onCancelLiked = async (pokemonId: string | number) => {
     if (user?.uid) {
