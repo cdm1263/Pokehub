bdc26bda0788275289fe24b3b9ac721db9290b7a
ChaDongmin <cdm1263@gmail.com>
2023-12-07 16:41:31 +0900

    Feat: 닉네임1,2 및 스테이터스 바 적용

--- src/components/card/CardEditor.tsx
+++ src/components/card/CardEditor.tsx
@@ -1,6 +1,31 @@
+import {
+  POKEMON_NICKNAME1,
+  POKEMON_NICKNAME2,
+  POKEMON_TYPES,
+} from '@/lib/constants';
 import styles from './cards.module.scss';
+import useSelectedPokemonForCard from '@/store/useSelectedPokemonForCard';
+import StatusBar from '../detail/StatusBar';
+import Plate from '../plate/Plate';
+import { TypesType } from '@/lib/type';
 
 const CardEditor = () => {
+  const {
+    pokemonData,
+    pokemonNickName1,
+    pokemonNickName2,
+    setPokemonNickName1,
+    setPokemonNickName2,
+  } = useSelectedPokemonForCard();
+
+  const getRandomNickName = (NickNames: string[], index: number) => {
+    const randomIndex = Math.floor(Math.random() * NickNames.length);
+    const randomNickName = NickNames[randomIndex];
+    index === 1
+      ? setPokemonNickName1(randomNickName)
+      : setPokemonNickName2(randomNickName);
+  };
+
   return (
     <div className={styles.editor_wrapper}>
       <div>
@@ -9,15 +34,29 @@ const CardEditor = () => {
           <li>
             <span className={styles.list_name}>별칭 1</span>
             <div>
-              <span>전설의</span>
-              <button className={styles.border_button}>랜덤</button>
+              <span>{pokemonNickName1}</span>
+              <button
+                className={styles.border_button}
+                onClick={() => {
+                  getRandomNickName(POKEMON_NICKNAME1, 1);
+                }}
+              >
+                랜덤
+              </button>
             </div>
           </li>
           <li>
             <span className={styles.list_name}>별칭 2</span>
             <div>
-              <span>화가난</span>
-              <button className={styles.border_button}>랜덤</button>
+              <span>{pokemonNickName2}</span>
+              <button
+                className={styles.border_button}
+                onClick={() => {
+                  getRandomNickName(POKEMON_NICKNAME2, 2);
+                }}
+              >
+                랜덤
+              </button>
             </div>
           </li>
           <li>
@@ -27,17 +66,73 @@ const CardEditor = () => {
         </ul>
       </div>
       <div>
-        <div>
+        <div className={styles.status_title}>
           <span className={styles.title}>스테이터스</span>
-          <ul>
-            <li>체력</li>
-            <li>공격</li>
-            <li>방어</li>
-            <li>특수공격</li>
-            <li>특수방어</li>
-            <li>스피드</li>
-          </ul>
+          <div>
+            {pokemonData?.types.map((pokemonType: TypesType) => (
+              <Plate
+                key={pokemonType.type.name}
+                pokemonTypeProp={POKEMON_TYPES[pokemonType.type.name]}
+              />
+            ))}
+          </div>
         </div>
+        <ul className={styles.status_bar_container}>
+          <li>
+            <span>체력</span>
+            {pokemonData ? (
+              <StatusBar
+                baseStat={pokemonData?.stats[0]}
+                pokemonTypes={pokemonData?.types}
+              />
+            ) : null}
+          </li>
+          <li>
+            <span>공격</span>
+            {pokemonData ? (
+              <StatusBar
+                baseStat={pokemonData?.stats[1]}
+                pokemonTypes={pokemonData?.types}
+              />
+            ) : null}
+          </li>
+          <li>
+            <span>방어</span>
+            {pokemonData ? (
+              <StatusBar
+                baseStat={pokemonData?.stats[2]}
+                pokemonTypes={pokemonData?.types}
+              />
+            ) : null}
+          </li>
+          <li>
+            <span>특수공격</span>
+            {pokemonData ? (
+              <StatusBar
+                baseStat={pokemonData?.stats[3]}
+                pokemonTypes={pokemonData?.types}
+              />
+            ) : null}
+          </li>
+          <li>
+            <span>특수방어</span>
+            {pokemonData ? (
+              <StatusBar
+                baseStat={pokemonData?.stats[4]}
+                pokemonTypes={pokemonData?.types}
+              />
+            ) : null}
+          </li>
+          <li>
+            <span>스피드</span>
+            {pokemonData ? (
+              <StatusBar
+                baseStat={pokemonData?.stats[5]}
+                pokemonTypes={pokemonData?.types}
+              />
+            ) : null}
+          </li>
+        </ul>
       </div>
     </div>
   );
