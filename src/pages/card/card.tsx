872ad821bdd52bd9d7e1e411bfb6783f680370b9
ChaDongmin <cdm1263@gmail.com>
2023-12-07 18:38:35 +0900

    Refactor: CardPage 컴포넌트 반환

--- src/pages/card/card.tsx
+++ src/pages/card/card.tsx
@@ -1,13 +1,9 @@
-import CardsRowLike from '@/components/card/CardsRowLike';
-import CardsRowRandom from '@/components/card/CardsRowRandom';
-import ProductCard from '@/components/card/ProductCard';
+import CardPage from '@/components/card/CardPage';
 
 const Card = () => {
   return (
     <>
-      <ProductCard />
-      <CardsRowLike />
-      <CardsRowRandom />;
+      <CardPage />
     </>
   );
 };
