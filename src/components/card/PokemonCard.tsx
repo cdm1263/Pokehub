aabbfe05cc26e9cbaaabb935c91040209709ec9b
BearHumanS <bye0651@naver.com>
2023-12-17 05:52:28 +0900

    Feat: 마이페이지에서 포켓몬 카드 스타일 변경 관련 수정

--- src/components/card/PokemonCard.tsx
+++ src/components/card/PokemonCard.tsx
@@ -6,33 +6,58 @@ import { POKEMON_NAME } from '@/lib/pokemonName';
 import { reverseObject } from '@/lib/utill/reverseObject';
 import StatusBar from '../detail/StatusBar';
 import useSelectedPokemonForCard from '@/store/useSelectedPokemonForCard';
+import { useLocation } from 'react-router-dom';
 
 interface PokemonCardProp {
   pokemonCardData: filteredPokemonData;
+  pokemonNickName?: {
+    pokemonNickName1: string;
+    pokemonNickName2: string;
+  };
+  isOpen?: boolean;
 }
 
-const PokemonCard = ({ pokemonCardData: data }: PokemonCardProp) => {
+const PokemonCard = ({
+  pokemonNickName,
+  pokemonCardData: data,
+  isOpen,
+}: PokemonCardProp) => {
+  const location = useLocation();
+  const isMyPage = location.pathname.includes('mypage');
+
   const { pokemonNickName1, pokemonNickName2 } = useSelectedPokemonForCard();
-  if (!Object.keys(data).length) {
+  if (!data || Object.keys(data).length === 0) {
     // 임시로 null처리
     // 추후 로딩 ui 등으로 처리하기
     return null;
   }
 
-  const firstType = data?.types[0].type.name;
+  const firstType = data?.types?.[0].type.name;
   const setClassName = (mainClassName: string) => {
-    return firstType
-      ? `${styles[mainClassName]} ${styles[POKEMON_TYPES[firstType]]}`
-      : styles[mainClassName];
+    const typeClass = firstType ? styles[POKEMON_TYPES[firstType]] : '';
+    if (isMyPage) {
+      return `${styles[`${mainClassName}__my`]} ${typeClass}`;
+    } else {
+      return `${styles[mainClassName]} ${typeClass}`;
+    }
   };
 
   return (
-    <div className={setClassName('wrapper')}>
+    <div
+      className={setClassName('wrapper')}
+      style={{ transform: isOpen ? 'scale(2.33)' : undefined }}
+    >
       <div className={setClassName('pokemon_number')}>{`No.${data?.id}`}</div>
       <div className={styles.white_block}>
         <div className={setClassName('container')}>
           <div className={styles.container__top}>
-            <div className={styles.type_containter}>
+            <div
+              className={
+                isMyPage
+                  ? `${styles.type_containter__my}`
+                  : `${styles.type_containter}`
+              }
+            >
               {data?.types.map((pokemonType: TypesType) => (
                 <Plate
                   key={pokemonType.type.name}
@@ -41,23 +66,65 @@ const PokemonCard = ({ pokemonCardData: data }: PokemonCardProp) => {
               ))}
             </div>
             <img
-              className={styles.pokemon_image}
+              className={
+                isMyPage
+                  ? `${styles.pokemon_image__my}`
+                  : `${styles.pokemon_image}`
+              }
               src={data.sprites}
               alt="포켓몬 이미지"
             />
             <div className={styles.pokemon_intro}>
-              <div className={styles.pokemon_name}>
-                <span className={styles.text__small}>{pokemonNickName1}</span>
-                <span className={styles.text__small}>{pokemonNickName2}</span>
+              <div
+                className={
+                  isMyPage
+                    ? `${styles.pokemon_name__my}`
+                    : `${styles.pokemon_name}`
+                }
+              >
+                <span
+                  className={
+                    isMyPage
+                      ? `${styles.text__small__my}`
+                      : `${styles.text__small}`
+                  }
+                >
+                  {pokemonNickName1 || pokemonNickName?.pokemonNickName1}
+                </span>
+                <span
+                  className={
+                    isMyPage
+                      ? `${styles.text__small__my}`
+                      : `${styles.text__small}`
+                  }
+                >
+                  {pokemonNickName2 || pokemonNickName?.pokemonNickName2}
+                </span>
               </div>
-              <span className={styles.text__large}>
+              <span
+                className={
+                  isMyPage
+                    ? `${styles.text__large__my}`
+                    : `${styles.text__large}`
+                }
+              >
                 {data && reverseObject(POKEMON_NAME)[data.name]}
               </span>
             </div>
           </div>
-          <div className={styles.container__bottom}>
+          <div
+            className={
+              isMyPage
+                ? `${styles.container__my__bottom}`
+                : `${styles.container__bottom}`
+            }
+          >
             <div>
-              <div className={styles.status}>
+              <div
+                className={
+                  isMyPage ? `${styles.status__my}` : `${styles.status}`
+                }
+              >
                 <span>체력</span>
                 {data ? (
                   <StatusBar
@@ -66,7 +133,11 @@ const PokemonCard = ({ pokemonCardData: data }: PokemonCardProp) => {
                   />
                 ) : null}
               </div>
-              <div className={styles.status}>
+              <div
+                className={
+                  isMyPage ? `${styles.status__my}` : `${styles.status}`
+                }
+              >
                 <span>특수공격</span>
                 {data ? (
                   <StatusBar
@@ -77,7 +148,11 @@ const PokemonCard = ({ pokemonCardData: data }: PokemonCardProp) => {
               </div>
             </div>
             <div>
-              <div className={styles.status}>
+              <div
+                className={
+                  isMyPage ? `${styles.status__my}` : `${styles.status}`
+                }
+              >
                 <span>공격</span>
                 {data ? (
                   <StatusBar
@@ -86,7 +161,11 @@ const PokemonCard = ({ pokemonCardData: data }: PokemonCardProp) => {
                   />
                 ) : null}
               </div>
-              <div className={styles.status}>
+              <div
+                className={
+                  isMyPage ? `${styles.status__my}` : `${styles.status}`
+                }
+              >
                 <span>특수방어</span>
                 {data ? (
                   <StatusBar
@@ -97,7 +176,11 @@ const PokemonCard = ({ pokemonCardData: data }: PokemonCardProp) => {
               </div>
             </div>
             <div>
-              <div className={styles.status}>
+              <div
+                className={
+                  isMyPage ? `${styles.status__my}` : `${styles.status}`
+                }
+              >
                 <span>방어</span>
                 {data ? (
                   <StatusBar
@@ -106,7 +189,11 @@ const PokemonCard = ({ pokemonCardData: data }: PokemonCardProp) => {
                   />
                 ) : null}
               </div>
-              <div className={styles.status}>
+              <div
+                className={
+                  isMyPage ? `${styles.status__my}` : `${styles.status}`
+                }
+              >
                 <span>스피드</span>
                 {data ? (
                   <StatusBar
@@ -118,13 +205,27 @@ const PokemonCard = ({ pokemonCardData: data }: PokemonCardProp) => {
             </div>
             <div>
               <img
-                className={styles.logo}
+                className={isMyPage ? `${styles.logo__my}` : `${styles.logo}`}
                 src="/src/assets/logo-pokehub.png"
                 alt="PoketHub"
               />
               <div className={styles.total_stat}>
-                <span className={styles.text__small}>Total</span>
-                <span className={styles.text__large}>
+                <span
+                  className={
+                    isMyPage
+                      ? `${styles.text__small__my}`
+                      : `${styles.text__small}`
+                  }
+                >
+                  Total
+                </span>
+                <span
+                  className={
+                    isMyPage
+                      ? `${styles.text__large__my}`
+                      : `${styles.text__large}`
+                  }
+                >
                   {data?.stats.reduce(
                     (acc, baseStat) => acc + baseStat.base_stat,
                     0,
