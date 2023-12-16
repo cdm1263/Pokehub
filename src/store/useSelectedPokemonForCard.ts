80ac129401486d1a175bc88d74d26dbebe1c5a84
ChaDongMin <cdm1263@gmail.com>
2023-12-16 14:19:49 +0900

    Feat: pokemonName, setPokemonName 추가

--- src/store/useSelectedPokemonForCard.ts
+++ src/store/useSelectedPokemonForCard.ts
@@ -4,6 +4,8 @@ import { create } from 'zustand';
 interface useSelectedPokemonForCard {
   pokemonData: PokemonType | null;
   setPokemonData: (data: PokemonType | null) => void;
+  pokemonName: string | null;
+  setPokemonName: (name: string) => void;
   pokemonNickName1: string | null;
   setPokemonNickName1: (nickName: string) => void;
   pokemonNickName2: string | null;
@@ -13,6 +15,8 @@ interface useSelectedPokemonForCard {
 const useSelectedPokemonForCard = create<useSelectedPokemonForCard>((set) => ({
   pokemonData: null,
   setPokemonData: (data) => set({ pokemonData: data }),
+  pokemonName: null,
+  setPokemonName: (name) => set({ pokemonName: name }),
   pokemonNickName1: null,
   setPokemonNickName1: (nickName) => set({ pokemonNickName1: nickName }),
   pokemonNickName2: null,
