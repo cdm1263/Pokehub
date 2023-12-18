8d12341103030d20b9a92074f668d79a255c1059
BearHumanS <bye0651@naver.com>
2023-12-19 01:29:33 +0900

    Feat: 모달에서 카드 이미지 다운로드 기능 추가 및 스타일 추가(스타일 임시, 수정해야함)

--- src/components/modal/CardModal.tsx
+++ src/components/modal/CardModal.tsx
@@ -1,16 +1,21 @@
 import PokemonCard from '../card/PokemonCard';
 import { Card } from '../mypage/Mycard';
 import styles from './CardModal.module.scss';
-import { MouseEvent } from 'react';
+import { MouseEvent, useRef } from 'react';
 import { LuPlus } from 'react-icons/lu';
+import domtoimage from 'dom-to-image';
+import { saveAs } from 'file-saver';
 
 interface CardModalProps {
+  tagName?: string;
   onModalToggle: (e: MouseEvent<HTMLDivElement>) => void;
   cardData: Card | null | undefined;
   isOpen?: boolean;
 }
 
 const CardModal = ({ onModalToggle, cardData, isOpen }: CardModalProps) => {
+  const cardRef = useRef<HTMLDivElement>(null);
+
   const card = cardData?.data;
   const pokemonNickName = {
     pokemonNickName1: card?.pokemonCardData[1],
@@ -21,20 +26,46 @@ const CardModal = ({ onModalToggle, cardData, isOpen }: CardModalProps) => {
     e.stopPropagation();
   };
 
+  const onDownloadBtn = () => {
+    const card = cardRef.current;
+    if (card) {
+      const filter = (card: Node) => {
+        return (card as HTMLElement).tagName !== 'BUTTON';
+      };
+      domtoimage.toBlob(card, { filter: filter }).then((blob) => {
+        saveAs(blob, 'card.png');
+      });
+    }
+  };
+
   return (
     <div className={styles.modal__container}>
       <div className={styles.modal__overlay} onClick={onModalToggle}>
-        <div className={styles.modal__contents} onClick={handleContentClick}>
-          <div className={styles.modal__contents__close}>
+        <div onClick={handleContentClick}>
+          <div className={styles.download__box}>
+            <button style={{ zIndex: '9999' }} onClick={onDownloadBtn}>
+              다운로드
+            </button>
+          </div>
+          <div className={styles.close__btn}>
             <div onClick={onModalToggle}>
               <LuPlus size={20} className={styles.rotate} />
             </div>
           </div>
-          <PokemonCard
-            isOpen={isOpen}
-            pokemonCardData={card?.pokemonCardData[0]}
-            pokemonNickName={pokemonNickName}
-          />
+          <div
+            ref={cardRef}
+            className={styles.modal__contents}
+            onClick={handleContentClick}
+          >
+            <div className={styles.modal__contents__close}></div>
+            <div className={styles.modal__contents}>
+              <PokemonCard
+                isOpen={isOpen}
+                pokemonCardData={card?.pokemonCardData[0]}
+                pokemonNickName={pokemonNickName}
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
