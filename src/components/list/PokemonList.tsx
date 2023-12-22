63d2201a92dec79fdf77a61f94192a01d644d34b
ChaDongmin <cdm1263@gmail.com>
2023-12-22 15:59:56 +0900

    Feat: 무한 스크롤로 초기 렌더링 수 제한하는 로직 구현

--- src/components/list/PokemonList.tsx
+++ src/components/list/PokemonList.tsx
@@ -1,7 +1,8 @@
+import { useState, useEffect } from 'react';
 import styles from './PokemonList.module.scss';
 import { useGetAllPokemon } from '@/query/qeuries';
 import { POKEMON_TYPES } from '@/lib/constants';
-import { TypesType } from '@/lib/type';
+import { PokemonType, TypesType } from '@/lib/type';
 import useSelectedStore from '@/store/useSelectedStore';
 import PokemonListElement from './PokemonListElement';
 import Inner from '../Inner';
@@ -10,37 +11,59 @@ import { POKEMON_NAME } from '@/lib/pokemonName';
 import { reverseObject } from '@/lib/utill/reverseObject';
 
 const PokemonList = () => {
-  const { isLoading, data: pokemonDatas } = useGetAllPokemon(1017);
+  const { data: allPokemonData } = useGetAllPokemon(1017);
   const { inputText } = useSearchInputText();
   const { selectedPlate } = useSelectedStore();
   const reversedPokemonNameObject = reverseObject(POKEMON_NAME);
-  const filteredData = pokemonDatas?.filter((data) => {
-    if (inputText) {
-      return reversedPokemonNameObject[data?.name]?.includes(inputText);
-    }
-    if (!selectedPlate.length) {
-      return data;
-    }
-    const { types } = data;
-    return selectedPlate.every((plate) =>
-      types.some((type: TypesType) => POKEMON_TYPES[type.type.name] === plate),
-    );
-  });
+  const [visibleData, setVisibleData] = useState<PokemonType[]>([]);
+  const [pageNum, setPageNum] = useState(1);
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+  useEffect(() => {
+    if (allPokemonData) {
+      const filteredData = allPokemonData?.filter((data) => {
+        if (inputText) {
+          return reversedPokemonNameObject[data?.name]?.includes(inputText);
+        }
+        if (!selectedPlate.length) {
+          return data;
+        }
+        const { types } = data;
+        return selectedPlate.every((plate) =>
+          types.some(
+            (type: TypesType) => POKEMON_TYPES[type.type.name] === plate,
+          ),
+        );
+      });
+
+      setVisibleData(filteredData ? filteredData.slice(0, pageNum * 20) : []);
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [allPokemonData, pageNum, selectedPlate]);
+
+  const handleScroll = () => {
+    const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+
+    if (scrollTop + clientHeight >= scrollHeight - 20) {
+      setPageNum((prevPageNum) => prevPageNum + 1);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
     <Inner>
       <ul className={styles.pokemon_grid}>
-        {filteredData?.length ? (
-          filteredData.map((data) => (
-            <PokemonListElement data={data} key={data.name} />
-          ))
-        ) : (
-          <span>데이터없음!!!!</span>
-        )}
+        {visibleData.length
+          ? visibleData.map((data) => (
+              <PokemonListElement data={data} key={data.name} />
+            ))
+          : null}
       </ul>
     </Inner>
   );
