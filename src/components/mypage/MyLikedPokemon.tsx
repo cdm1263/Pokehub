4eb51558f804216ea9187c217b58d93f8411072e
BearHumanS <bye0651@naver.com>
2024-01-03 09:49:18 +0900

    Feat: 필요 없는 로직 수정

--- src/components/mypage/MyLikedPokemon.tsx
+++ src/components/mypage/MyLikedPokemon.tsx
@@ -1,32 +1,14 @@
-import { arrayRemove, doc, onSnapshot } from 'firebase/firestore';
+import { arrayRemove } from 'firebase/firestore';
 import styles from './Mypage.module.scss';
 import useUserStore from '@/store/useUsersStore';
 import { useNavigate } from 'react-router-dom';
 import useLikedStore from '@/store/useLikedStore';
 import RenderPokemon from './RenderPokemon';
 import { updateDocument } from '@/lib/firebaseQuery';
-import { useEffect } from 'react';
-import { db } from '@/firebase';
-import { getPokemonData } from '@/lib/poketApi';
 
 const MyLikedPokemon = () => {
   const { user } = useUserStore();
-  const { pokemonData, setPokemonData } = useLikedStore();
-
-  useEffect(() => {
-    if (user?.uid) {
-      const unsubscribe = onSnapshot(
-        doc(db, 'likes', user.uid),
-        async (doc) => {
-          const likedPokemons = doc.data()?.pokemons || [];
-          const data = await Promise.all(likedPokemons.map(getPokemonData));
-          setPokemonData(data);
-        },
-      );
-
-      return () => unsubscribe();
-    }
-  }, [user?.uid, setPokemonData]);
+  const { pokemonData } = useLikedStore();
 
   const navigate = useNavigate();
 
