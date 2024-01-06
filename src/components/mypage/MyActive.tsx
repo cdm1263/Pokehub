804a3c076354e4e2a9c3c2300f9438093de7a659
BearHumanS <bye0651@naver.com>
2024-01-06 14:39:29 +0900

    Feat: 작성한 게시물 컴포넌트 추가

--- src/components/mypage/MyActive.tsx
+++ src/components/mypage/MyActive.tsx
@@ -1,11 +1,14 @@
 import MyLikedPokemon from './MyLikedPokemon';
+import MyPosts from './MyPosts';
 import styles from './Mypage.module.scss';
 const MyActive = () => {
   return (
     <>
       <div className={styles.myactive__total__container}>
         <div className={styles.myactive__left__container}>
-          <div className={styles.myactive__posts}></div>
+          <div className={styles.myactive__posts}>
+            <MyPosts />
+          </div>
           <div className={styles.myactive__posts__favorite}></div>
         </div>
         <MyLikedPokemon />
