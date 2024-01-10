e714def1bf5088bbd7bf6afe563debb4e7529469
DICEPT <dicepted@gmail.com>
2024-01-10 18:15:10 +0900

    Feat: 날짜 생성 컴포넌트 기능 수정

--- src/lib/util/convertTime.ts
+++ src/lib/util/convertTime.ts
@@ -4,19 +4,31 @@ interface reverseObject {
 
 export const ConvertTime = (value: reverseObject) => {
   const utcDate = new Date(value.data);
-  const koreanDate = new Date(utcDate.getTime() + 9 * 60 * 60 * 1000);
-  const formattedKoreanDate = koreanDate.toISOString().split('T')[0];
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    timeZone: 'Asia/Seoul', // 시간대를 서울로 설정
+  };
+
+  const formattedKoreanDate = utcDate.toLocaleDateString('ko-KR', options);
 
   return formattedKoreanDate;
 };
 
 export const ConvertTimes = (value: reverseObject) => {
   const utcDate = new Date(value.data);
-  const koreanDate = new Date(utcDate.getTime() + 9 * 60 * 60 * 1000);
-  const formattedKoreanDates = koreanDate
-    .toISOString()
-    .replace(/T/, ' ')
-    .replace(/\.\d+Z$/, '');
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    timeZone: 'Asia/Seoul', // 시간대를 서울로 설정
+  };
 
-  return formattedKoreanDates;
+  const formattedKoreanDate = utcDate.toLocaleDateString('ko-KR', options);
+
+  return formattedKoreanDate;
 };
