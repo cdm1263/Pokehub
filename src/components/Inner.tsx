6e152df9d9a7537d57660e77ab2afbeab2b739e0
BearHumanS <bye0651@naver.com>
2024-01-12 16:37:35 +0900

    Feat: Inner style props추가 (인라인 스타일 적용하기 위해서)

--- src/components/Inner.tsx
+++ src/components/Inner.tsx
@@ -1,12 +1,17 @@
-import { ReactNode } from 'react';
+import { CSSProperties, ReactNode } from 'react';
 import styles from './Inner.module.scss';
 
 interface InnerProp {
   children: ReactNode;
+  style?: CSSProperties;
 }
 
-const Inner = ({ children }: InnerProp) => {
-  return <div className={styles.inner}>{children}</div>;
+const Inner = ({ children, style }: InnerProp) => {
+  return (
+    <div className={styles.inner} style={style}>
+      {children}
+    </div>
+  );
 };
 
 export default Inner;
