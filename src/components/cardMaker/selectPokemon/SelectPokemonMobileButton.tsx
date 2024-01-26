814bb54a7ac13c39c5cf68ee32c7f6bc0f031d44
ChaDongmin <cdm1263@gmail.com>
2024-01-26 19:11:13 +0900

    Feat: 모바일 사이즈용 포켓몬 선택 버튼 컴포넌트 생성

---
+++ src/components/cardMaker/selectPokemon/SelectPokemonMobileButton.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import styles from './select.module.scss';
+import { Modalportal } from '@/portal';
+import SelectModal from '@/components/modal/SelectModal';
+
+interface SelectPokemonMobileButtonProps {
+  buttonData: ButtonData;
+}
+
+interface ButtonData {
+  url: string;
+  title: string;
+  description: string;
+}
+
+const SelectPokemonMobileButton = ({
+  buttonData,
+}: SelectPokemonMobileButtonProps) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const { url, title, description } = buttonData;
+
+  const onClick = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  return (
+    <>
+      <button className={styles.selector_button} onClick={onClick}>
+        <div className={styles.img_wrapper}>
+          <img src={url} alt="포켓몬 이미지" />
+        </div>
+        <div className={styles.text_wrapper}>
+          <span>{title}</span>
+          <p>{description}</p>
+        </div>
+      </button>
+      {isOpen ? (
+        <Modalportal>
+          <SelectModal onClick={onClick} title={title} />
+        </Modalportal>
+      ) : null}
+    </>
+  );
+};
+
+export default SelectPokemonMobileButton;
