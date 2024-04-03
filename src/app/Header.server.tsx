77f6565919d37f827e97de7be10b76cb0d795aec
BearHumanS <bye0651@naver.com>
2024-04-04 04:07:02 +0900

    Refactor: 서버 컴포넌트와 클라이언트 컴포넌트 분리

---
+++ src/app/Header.server.tsx
@@ -0,0 +1,39 @@
+import Image from 'next/image';
+import SearchInput from '@/components/search/Search';
+import MobileNavUI from '@/components/header/MobileNavUI';
+import MobileNavToggle from '@/components/header/MobileNavToggle';
+import HeaderClient from '@/components/header/Header.client';
+import styles from '@/components/header/Header.module.scss';
+
+const Header = () => {
+  return (
+    <>
+      <header className={styles.main__header}>
+        <div className={styles.header__section}>
+          <div>
+            <div className={styles.logo}>
+              <a href="/">
+                <Image
+                  src="/logo-pokehub.png"
+                  alt="logo"
+                  width={132}
+                  height={59}
+                />
+              </a>
+            </div>
+            <div className={styles.menu__group}>
+              <SearchInput />
+              <HeaderClient />
+            </div>
+
+            <MobileNavUI />
+          </div>
+        </div>
+      </header>
+
+      <MobileNavToggle />
+    </>
+  );
+};
+
+export default Header;
