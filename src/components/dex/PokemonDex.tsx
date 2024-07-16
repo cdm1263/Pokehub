b4ef9cefe37cc7203749f15e5dbbabf93db78235
ChaDongmin <cdm1263@gmail.com>
2024-07-16 19:36:38 +0900

    Refactor: pokemon grid의 사이즈를 측정하는 사이드 이펙트 및 커스텀훅 적용

--- src/components/dex/PokemonDex.tsx
+++ src/components/dex/PokemonDex.tsx
@@ -4,10 +4,39 @@ import Inner from '../Inner';
 import useFilteredPokemonData from '@/hook/useFilteredPokemonData';
 import useVisibleDataByScroll from '@/hook/useVisibleDataByScroll';
 import Loading from '../loading/Loading';
+import { useLayoutEffect, useState } from 'react';
+import useElementCount from '@/hook/useElementCount';
+import useGetGridElementWidth from '@/hook/useGetGridElementWidth';
 
 const PokemonDex = () => {
   const { filteredData, isLoading } = useFilteredPokemonData();
-  const visibleData = useVisibleDataByScroll(filteredData);
+  const [ulWidth, setUlWidth] = useState(0);
+  const itemWidth = useGetGridElementWidth(ulWidth);
+  const { ulRef, elementCount } = useElementCount(itemWidth, 315, 20);
+
+  useLayoutEffect(() => {
+    if (!ulRef.current) return;
+
+    const handleResize = (entries: ResizeObserverEntry[]) => {
+      for (let entry of entries) {
+        if (entry.target === ulRef.current) {
+          const { width } = entry.contentRect;
+          setUlWidth(width);
+        }
+      }
+    };
+
+    const resizeObserver = new ResizeObserver(handleResize);
+    resizeObserver.observe(ulRef.current);
+
+    return () => {
+      if (ulRef.current) {
+        resizeObserver.unobserve(ulRef.current);
+      }
+    };
+  }, [filteredData]);
+
+  const visibleData = useVisibleDataByScroll(filteredData, elementCount.total);
 
   if (isLoading) {
     return <Loading />;
@@ -15,7 +44,7 @@ const PokemonDex = () => {
 
   return (
     <Inner>
-      <ul className={styles.pokemon_grid}>
+      <ul className={styles.pokemon_grid} ref={ulRef}>
         {visibleData.length
           ? visibleData.map((data) => (
               <PokemonDexElement data={data} key={data.name} />
